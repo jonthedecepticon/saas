f464512325fe2a91139248037b4a74a33dcada25
Jon Lambson <jon@crowdengine.com>
2016-04-17 00:17:09 -0600

    added registrations

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+  // Watch for a form submission:
+  $("#form-submit-btn").click(function(event) {
+    event.preventDefault();
+    $('input[type=submit]').prop('disabled', true);
+    var error = false;
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
+    if (!error) {
+      // Get the Stripe token:
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    return false;
+  }); // form submission
+  function stripeResponseHandler(status, response) {
+    var $form = $('#payment-form');
+
+    if (response.error) {
+      // Show the errors on the form
+      $form.find('.payment-errors').text(response.error.message);
+      $form.find('button').prop('disabled', false);
+    } else {
+      // response contains id and card, which contains additional card details
+      var token = response.id;
+      // Insert the token into the form so it gets submitted to the server
+      $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+      // and submit
+      $form.get(0).submit();
+    }
+  };
+});
